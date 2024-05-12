4664f5e7c9422bc72da1777b1e834ff22979b020
Prathmesh3080 <prathmeshpatil843@gmail.com>
2024-05-12 23:11:45 +0530

    Update
    Update and correct the API Endpoints

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -10,18 +10,18 @@ export class EmployeeService {
   constructor(private http: HttpClient) {}
 
   addEmployee(data: any): Observable<any> {
-    return this.http.post('http://localhost:3000/employees', data);
+    return this.http.post('http://localhost:3000/employee', data);
   }
 
   updateEmployee(id: number, data: any): Observable<any> {
-    return this.http.put(`http://localhost:3000/employees/${id}`, data);
+    return this.http.put(`http://localhost:3000/employee/${id}`, data);
   }
 
   getEmployeeList(): Observable<any> {
-    return this.http.get('http://localhost:3000/employees');
+    return this.http.get('http://localhost:3000/employee');
   }
 
   deleteEmployee(id: number): Observable<any> {
-    return this.http.delete(`http://localhost:3000/employees/${id}`);
+    return this.http.delete(`http://localhost:3000/employee/${id}`);
   }
 }
